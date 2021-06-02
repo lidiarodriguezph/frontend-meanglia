6c16d31677fc6323066ed365e88899f3ad7136d3
Tu nombre <lirodriguez030898@gmail.com>
2021-06-02 13:35:29 -0400

    lidia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { AdminModule } from './@admin/pages/admin.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicModule } from './@public/pages/public.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AdminModule,
+    PublicModule,
     AppRoutingModule
   ],
   providers: [],
